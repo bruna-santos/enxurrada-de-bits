d1c71a86a04b1d1cbb95702d8bf071ee30787b3c
bruna-santos <70449858+bruna-santos@users.noreply.github.com>
2020-10-30 16:21:58 -0300

    finalizando arquivos java aula 7 e 8
    bug calculadora consertado

---
+++ aula 7 - js/javascript.js
@@ -0,0 +1,6 @@
+let anoAtual = '2020';
+let nome, idade, texto;
+nome = prompt('Digite seu nome: ')
+let anoNascimento = prompt('Digite seu ano de nascimento: ')
+idade = anoAtual-anoNascimento;
+texto = alert(`Olá, seu nome é ${nome} e você tem ${idade} anos`);
