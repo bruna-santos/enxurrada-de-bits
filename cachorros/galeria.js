fb5dc25d0cfa3aedc5663457a9f0038e989b6284
bruna-santos <70449858+bruna-santos@users.noreply.github.com>
2020-12-03 13:05:50 -0300

    adicionando pasta cachorros

---
+++ cachorros/galeria.js
@@ -0,0 +1,211 @@
+let imagemAtual = 0;
+let beagle = document.querySelector('img#beagle');
+let buldogue = document.querySelector('img#buldogue');
+let chiuaua = document.querySelector('img#chiuaua');
+let quasebasset = document.querySelector('img#quasebasset');
+let golden = document.querySelector('img#golden');
+let labrador = document.querySelector('img#labrador');
+let lhasa = document.querySelector('img#lhasa');
+let maltes = document.querySelector('img#maltes');
+let pastor = document.querySelector('img#pastor');
+let poodle = document.querySelector('img#poodle');
+let pug = document.querySelector('img#pug');
+let shihtzu = document.querySelector('img#shihtzu');
+let schnauzer = document.querySelector('img#schnauzer');
+let viraLata = document.querySelector('img#viralata');
+let yorkshire = document.querySelector('img#yorkshire');
+
+function verBeagle() {
+    if (imagemAtual === 0) {
+        beagle.src="../raças/beagle.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        beagle.src ="../raças/beagle2.png";
+        imagemAtual = 2;
+    } else {
+        beagle.src ="../raças/beagle3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verBuldogue() {
+    if (imagemAtual === 0) {
+        buldogue.src="../raças/buldogue1.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        buldogue.src ="../raças/buldogue2.png";
+        imagemAtual = 2;
+    } else {
+        buldogue.src ="../raças/buldogue3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verChiuaua() {
+    if (imagemAtual === 0) {
+        chiuaua.src="../raças/chiuaua.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        chiuaua.src ="../raças/chiuaua2.png";
+        imagemAtual = 2;
+    } else {
+        chiuaua.src ="../raças/chiuaua3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verQuaseBasset() {
+    if (imagemAtual === 0) {
+        quasebasset.src="../raças/quasebasset.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        quasebasset.src ="../raças/quasebasset2.png";
+        imagemAtual = 2;
+    } else {
+        quasebasset.src ="../raças/quasebasset3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verGolden() {
+    if (imagemAtual === 0) {
+        golden.src="../raças/golden.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        golden.src ="../raças/golden2.png";
+        imagemAtual = 2;
+    } else {
+        golden.src ="../raças/goldenpersonalidade.png";
+        imagemAtual = 0;
+    }
+}
+
+function verLabrador() {
+    if (imagemAtual === 0) {
+        labrador.src="../raças/labrador2.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        labrador.src ="../raças/labrador1.png";
+        imagemAtual = 2;
+    } else {
+        labrador.src ="../raças/labrador3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verLhasa() {
+    if (imagemAtual === 0) {
+        lhasa.src="../raças/lhasa.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        lhasa.src ="../raças/lhasa1.png";
+        imagemAtual = 2;
+    } else {
+        lhasa.src ="../raças/lhasa3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verMaltes() {
+    if (imagemAtual === 0) {
+        maltes.src="../raças/maltes1.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        maltes.src ="../raças/maltes.png";
+        imagemAtual = 2;
+    } else {
+        maltes.src ="../raças/maltes4.png";
+        imagemAtual = 0;
+    }
+}
+
+function verPastor() {
+    if (imagemAtual === 0) {
+        pastor.src="../raças/pastor.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        pastor.src ="../raças/pastor2.png";
+        imagemAtual = 2;
+    } else {
+        pastor.src ="../raças/pastor3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verPoodle() {
+    if (imagemAtual === 0) {
+        poodle.src="../raças/poodle.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        poodle.src ="../raças/poodle3.png";
+        imagemAtual = 2;
+    } else {
+        poodle.src ="../raças/poodle4.png";
+        imagemAtual = 0;
+    }
+}
+
+function verPug() {
+    if (imagemAtual === 0) {
+        pug.src="../raças/pug1.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        pug.src ="../raças/pug2.png";
+        imagemAtual = 2;
+    } else {
+        pug.src ="../raças/pug3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verShihtzu() {
+    if (imagemAtual === 0) {
+        shihtzu.src="../raças/shihtzu3.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        shihtzu.src ="../raças/shihtzu.png";
+        imagemAtual = 2;
+    } else {
+        shihtzu.src ="../raças/shihtzu4.png";
+        imagemAtual = 0;
+    }
+}
+
+function verSchnauzer() {
+    if (imagemAtual === 0) {
+        schnauzer.src="../raças/schnauzer.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        schnauzer.src ="../raças/schnauzer2.png";
+        imagemAtual = 2;
+    } else {
+        schnauzer.src ="../raças/schnauzer3.png";
+        imagemAtual = 0;
+    }
+}
+
+function verViraLata() {
+    if (imagemAtual === 0) {
+        viraLata.src="../raças/viralata1.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        viraLata.src ="../raças/viralata4.png";
+        imagemAtual = 2;
+    } else {
+        viraLata.src ="../raças/viralata.png";
+        imagemAtual = 0;
+    }
+}
+
+function verYorkshire() {
+    if (imagemAtual === 0) {
+        yorkshire.src="../raças/yorkshire.png";
+        imagemAtual = 1;
+    } else if (imagemAtual === 1) {
+        yorkshire.src ="../raças/yorkshire1.png";
+        imagemAtual = 2;
+    } else {
+        yorkshire.src ="../raças/yorkshire2.png";
+        imagemAtual = 0;
+    }
+}
