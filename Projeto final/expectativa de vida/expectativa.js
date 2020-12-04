dcaa662901ca95b1646f389e6961d8ff456e9822
bruna-santos <70449858+bruna-santos@users.noreply.github.com>
2020-12-04 19:58:37 -0300

    atualizando js de expectativa de vida

--- Projeto final/expectativa de vida/expectativa.js
+++ Projeto final/expectativa de vida/expectativa.js
@@ -8,6 +8,8 @@ const grande = 12;
 let a, idadeTotal, x, y, z, s;
 let resultado = document.querySelector('#resultadoFinal');
 let text = document.createElement('p');
+let triste = document.querySelector('#triste');
+let pesames = document.createElement('p');
 let i = 0;
 function clicouNoBotao(){
         if (genero.value === 'macho') {
@@ -23,37 +25,37 @@ function clicouNoBotao(){
         } else if (genero.value === 'none'){
             i++;
         }
+
        if (d.value === 'pequeno') {
-        idadeTotal = parseInt(pequeno) - parseInt(c.value);
-        resultado.innerHTML = (`Como ${a} ${nome.value} é de porte ${d.value}, sua expectativa de vida é de 16 anos e por isso seu pet provavelmente viverá mais ${parseInt(idadeTotal)} anos.`);
+        idadeTotal = parseInt(pequeno) - parseInt(c.value);   
         y = 16;
-    }   if (d.value === 'grande') {
+        }   
+        if (d.value === 'grande') {
         idadeTotal = parseInt(grande) - parseInt(c.value);
-        resultado.innerHTML = (`Como ${a} ${nome.value} é de porte ${d.value}, sua expectativa de vida é de 12 anos e por isso seu pet provavelmente viverá mais ${parseInt(idadeTotal)} anos.`);
         y = 12;
-    } else if (d.value === 'médio') {
+        } 
+        else if (d.value === 'médio') {
         idadeTotal = parseInt(medio) - parseInt(c.value);
-        resultado.innerHTML = (`Como ${a} ${nome.value} é de porte ${d.value}, sua expectativa de vida é de 14 anos e por isso seu pet provavelmente viverá mais ${parseInt(idadeTotal)} anos.`);
         y = 14;
-    } else if (d.value === 'none') {
+        } 
+        else if (d.value === 'none') {
         i++;
-    }
-        if (parseInt(idadeTotal) < 3) {
-        triste.innerHTML = (`Infelizmente ${a} ${nome.value} tem apenas alguns aninhos restantes :( <br> Aproveite ao máximo seu tempo com el${x}!`)
-    }
-        if (parseInt(i) > 0){
+        }
+        if (parseInt(idadeTotal) < 0){
+            triste.innerHTML = (`A expectativa de vida de cães de porte ${d.value} geralmente é ${y} anos. ${z} ${nome.value} já ultrapassou essa média. Isso mostra que el${x} é um pet muito saudável, mas que provavelmente não tem mais muito tempo :( <br> Por isso, aproveite ao máximo seu tempo com ${a} ${s} companheir${a}!`);
+        }   if (parseInt(idadeTotal) === 0){
+            triste.innerHTML = (`A expectativa de vida de cães de porte ${d.value} geralmente é ${y} anos. ${z} ${nome.value} já alcançou essa faixa etária. Isso mostra que el${x} é um pet saudável, mas que provavelmente não tem mais muito tempo :( <br> Por isso, aproveite ao máximo seu tempo com ${a} ${s} companheir${a}!`);
+        }   if (parseInt(idadeTotal) === 3){
+            triste.innerHTML = (`A expectativa de vida de cães de porte ${d.value} geralmente é ${y} anos. ${z} ${nome.value} está próxim${a} dessa faixa etária. Por isso, infelizmente, ${a} ${nome.value} tem apenas alguns aninhos restantes :( <br> Aproveite ao máximo seu tempo com el${x}!`);
+        }   if (parseInt(idadeTotal) === 2){
+            triste.innerHTML = (`A expectativa de vida de cães de porte ${d.value} geralmente é ${y} anos. ${z} ${nome.value} está próxim${a} dessa faixa etária. Por isso, infelizmente, ${a} ${nome.value} tem apenas alguns aninhos restantes :( <br> Aproveite ao máximo seu tempo com el${x}!`);
+        }   if (parseInt(idadeTotal) === 1){
+            triste.innerHTML = (`A expectativa de vida de cães de porte ${d.value} geralmente é ${y} anos. ${z} ${nome.value} está próxim${a} dessa faixa etária. Por isso, infelizmente, ${a} ${nome.value} tem apenas alguns aninhos restantes :( <br> Aproveite ao máximo seu tempo com el${x}!`);
+        }   if (parseInt(idadeTotal) > 3){
+            triste.innerHTML = (`A expectativa de vida de cães de porte ${d.value} geralmente é ${y} anos. Por isso, ${a} ${nome.value} provavelmente viverá mais ${parseInt(idadeTotal)} anos.`)
+        }   if (parseInt(i) > 0){
             text.innerHTML = ('É necessário preencher <strong>TODOS</strong> os campos para realizar a consulta.');
             text.innerHTML = "";
             resultado.appendChild(text);
     }
-        if (parseInt(idadeTotal) < 0){
-            text.innerHTML = (`A expectativa de vida de cães de porte ${d.value} geralmente é ${y} anos. ${z} ${nome.value} já ultrapassou essa média. Isso mostra que el${x} é um pet muito saudável, mas que provavelmente não tem mais muito tempo :( <br> Por isso, aproveite ao máximo seu tempo com ${a} ${s} companheir${a}!`);
-            text.innerHTML = "";
-            resultado.appendChild(text);
-        }
-        if (parseInt(idadeTotal) === 0){
-            text.innerHTML = (`A expectativa de vida de cães de porte ${d.value} geralmente é ${y} anos. ${z} ${nome.value} já alcançou essa faixa etária. Isso mostra que el${x} é um pet saudável, mas que provavelmente não tem mais muito tempo :( <br> Por isso, aproveite ao máximo seu tempo com ${a} ${s} companheir${a}!`);
-            text.innerHTML = "";
-            resultado.appendChild(text);
-        }
 }
